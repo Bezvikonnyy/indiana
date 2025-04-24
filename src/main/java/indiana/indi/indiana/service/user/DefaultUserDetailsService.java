a2a7de530fc3363266ae0ae1700aff43b2a1122e
Bezvikonnyy <120276322+Bezvikonnyy@users.noreply.github.com>
2025-04-24 18:55:40 +0300

    Добавил конфиги безопасности, сущности User/Role, обновил pom.xml

---
+++ src/main/java/indiana/indi/indiana/service/user/DefaultUserDetailsService.java
@@ -0,0 +1,70 @@
+package indiana.indi.indiana.service.user;
+
+import indiana.indi.indiana.entity.Role;
+import indiana.indi.indiana.entity.User;
+import indiana.indi.indiana.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class DefaultUserDetailsService implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public User createUser(String username, String password, Set<Role> roles) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRoles(roles);
+        return this.userRepository.save(user);
+    }
+
+    @Override
+    public User editUser(Long id, String username, String password, Set<Role> roles) throws UsernameNotFoundException{
+        return this.userRepository.findById(id).map(
+                user -> {
+                    user.setUsername(username);
+                    user.setPassword(passwordEncoder.encode(password));
+                    user.setRoles(roles);
+
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UsernameNotFoundException("User not found!" + id));
+    }
+
+    @Override
+    public User getUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Override
+    public void deleteUser(Long id) throws UsernameNotFoundException {
+        if (!userRepository.existsById(id)) {
+            throw new UsernameNotFoundException("User with id " + id + " not found");
+        }
+        this.userRepository.deleteById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map( user -> new org.springframework.security.core.userdetails.User(
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.getRoles().stream()
+                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getTitle()))
+                            .collect(Collectors.toList())
+        )).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
